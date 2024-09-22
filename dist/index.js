b85234c7b07a7ad6e3c03be303c255068c56cf68
kurniawanxyz <kaitodecode@gmail.com>
2024-09-22 12:20:13 +0700

    add example code into file

--- dist/index.js
+++ dist/index.js
@@ -28,6 +28,57 @@ exports.createFile = createFile;
 const fs = __importStar(require("fs-extra"));
 const path = __importStar(require("path"));
 const baseDir = process.cwd(); // Mengambil direktori kerja saat ini
+const exampleCodes = {
+    service: (fileName) => `
+import {} from "../repositories"
+// Contoh kode untuk service: ${fileName}Service\nexport function ${fileName}Service() {\n  // Implementasi service\n}`,
+    handler: (fileName) => `
+import { Hono, Context } from 'hono';
+
+// Contoh kode untuk handler: ${fileName}Handler
+const app = new Hono();
+app.get('/', (c: Context) => {
+  c.text('Hello, World!');
+});
+  `,
+    domain: (fileName) => `
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
+
+// Contoh kode untuk domain: ${fileName}Domain
+export const ${fileName}Domain = z.object({
+
+})
+export type ${fileName} = z.infer<typeof ${fileName}Domain>
+;
+  `,
+    repository: (fileName) => `
+const prisma = new PrismaClient();
+
+// Contoh kode untuk repository: ${fileName}Repository
+export const create${fileName}Repository = async (data: any) => {
+  return await prisma.example.create({ data });
+};
+
+export const read${fileName}Repository = async (id: number) => {
+  return await prisma.example.findUnique({ where: { id } });
+};
+
+export const update${fileName}Repository = async (id: number, data: any) => {
+  return await prisma.example.update({ where: { id }, data });
+};
+
+export const delete${fileName}Repository = async (id: number) => {
+  return await prisma.example.delete({ where: { id } });
+};
+
+export const list${fileName}Repository = async () => {
+  return await prisma.example.findMany();
+};
+  `,
+    helpers: (fileName) => `// Contoh kode untuk helper: ${fileName}Helper\nexport function ${fileName}Helper() {\n  // Implementasi helper\n}`,
+    lib: (fileName) => `// Contoh kode untuk lib: ${fileName}Lib\nexport function ${fileName}Lib() {\n  // Implementasi lib\n}`
+};
 function initializeProjectStructure() {
     const directories = ['domains', 'helpers', 'services', 'handlers', 'libs', 'repositories']; // Folder yang akan dibuat
     directories.forEach(dir => {
@@ -46,29 +97,39 @@ function initializeProjectStructure() {
 async function createFile(option, fileName) {
     try {
         let dirPaths = [];
+        let fullFileName = '';
         // Menentukan direktori berdasarkan opsi
         switch (option) {
             case 'service':
                 dirPaths.push(path.join(baseDir, 'src/services'));
+                fullFileName = `${fileName}Service`;
                 break;
             case 'handler':
                 dirPaths.push(path.join(baseDir, 'src/handlers'));
+                fullFileName = `${fileName}Handler`;
                 break;
             case 'domain':
                 dirPaths.push(path.join(baseDir, 'src/domains'));
+                fullFileName = `${fileName}Domain`;
                 break;
             case 'repository':
                 dirPaths.push(path.join(baseDir, 'src/repositories'));
+                fullFileName = `${fileName}Repository`;
                 break;
             case 'helpers':
                 dirPaths.push(path.join(baseDir, 'src/helpers'));
+                fullFileName = `${fileName}Helper`;
                 break;
             case 'lib':
                 dirPaths.push(path.join(baseDir, 'src/lib'));
+                fullFileName = `${fileName}Lib`;
                 break;
             case 'resources':
-                dirPaths.push(path.join(baseDir, 'src/domains'), path.join(baseDir, 'src/handlers'), path.join(baseDir, 'src/repositories'), path.join(baseDir, 'src/services'));
-                break;
+                const resourceOptions = ['service', 'handler', 'domain', 'repository'];
+                for (const resourceOption of resourceOptions) {
+                    await createFile(resourceOption, fileName);
+                }
+                return;
             default:
                 console.error('Invalid option. Please use "service", "handler", "domain", "repository", "helpers", "lib", or "resources".');
                 return;
@@ -78,10 +139,10 @@ async function createFile(option, fileName) {
             // Memastikan direktori untuk file sudah ada
             await fs.ensureDir(dirPath);
             // Menentukan path lengkap untuk file
-            const filePath = path.join(dirPath, `${fileName}.ts`);
+            const filePath = path.join(dirPath, `${fullFileName}.ts`);
             // Menulis konten ke file
-            await fs.writeFile(filePath, `// File ${option.charAt(0).toUpperCase() + option.slice(1)}: ${fileName}`, 'utf-8');
-            console.log(`File ${option} sudah dibuat: ${fileName}.ts di ${dirPath}`);
+            await fs.writeFile(filePath, exampleCodes[option](fileName), 'utf-8');
+            console.log(`File ${option} sudah dibuat: ${fullFileName}.ts di ${dirPath}`);
         }
     }
     catch (err) {
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,61 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 
 const baseDir = process.cwd(); // Mengambil direktori kerja saat ini
+
+const exampleCodes: { [key: string]: (fileName: string) => string } = {
+  service: (fileName: string) => `
+import {} from "../repositories"
+// Contoh kode untuk service: ${fileName}Service\nexport function ${fileName}Service() {\n  // Implementasi service\n}`,
+  handler: (fileName: string) => `
+import { Hono, Context } from 'hono';
+
+// Contoh kode untuk handler: ${fileName}Handler
+const app = new Hono();
+app.get('/', async(c: Context) => {
+  c.text('Hello, World!');
+});
+  `,
+  domain: (fileName: string) => `
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
+
+// Contoh kode untuk domain: ${fileName}Domain
+export const ${fileName}Domain = z.object({
+
+})
+export type ${fileName} = z.infer<typeof ${fileName}Domain>
+;
+  `,
+  repository: (fileName: string) => `
+const prisma = new PrismaClient();
+
+// Contoh kode untuk repository: ${fileName}Repository
+export const create${fileName}Repository = async (data: any) => {
+  return await prisma.example.create({ data });
+};
+
+export const read${fileName}Repository = async (id: number) => {
+  return await prisma.example.findUnique({ where: { id } });
+};
+
+export const update${fileName}Repository = async (id: number, data: any) => {
+  return await prisma.example.update({ where: { id }, data });
+};
+
+export const delete${fileName}Repository = async (id: number) => {
+  return await prisma.example.delete({ where: { id } });
+};
+
+export const list${fileName}Repository = async () => {
+  return await prisma.example.findMany();
+};
+  `,
+  helpers: (fileName: string) => `// Contoh kode untuk helper: ${fileName}Helper\nexport function ${fileName}Helper() {\n  // Implementasi helper\n}`,
+  lib: (fileName: string) => `// Contoh kode untuk lib: ${fileName}Lib\nexport function ${fileName}Lib() {\n  // Implementasi lib\n}`
+};
+
 export function initializeProjectStructure() {
-  const directories = ['domains', 'helpers', 'services', 'handlers', 'libs','repositories']; // Folder yang akan dibuat
+  const directories = ['domains', 'helpers', 'services', 'handlers', 'libs', 'repositories']; // Folder yang akan dibuat
 
   directories.forEach(dir => {
     const dirPath = path.join(`${baseDir}/src/`, dir);
@@ -24,35 +77,40 @@ export function initializeProjectStructure() {
 export async function createFile(option: string, fileName: string) {
   try {
     let dirPaths: string[] = [];
-    
+    let fullFileName: string = '';
+
     // Menentukan direktori berdasarkan opsi
     switch (option) {
       case 'service':
         dirPaths.push(path.join(baseDir, 'src/services'));
+        fullFileName = `${fileName}Service`;
         break;
       case 'handler':
         dirPaths.push(path.join(baseDir, 'src/handlers'));
+        fullFileName = `${fileName}Handler`;
         break;
       case 'domain':
         dirPaths.push(path.join(baseDir, 'src/domains'));
+        fullFileName = `${fileName}Domain`;
         break;
       case 'repository':
         dirPaths.push(path.join(baseDir, 'src/repositories'));
+        fullFileName = `${fileName}Repository`;
         break;
       case 'helpers':
         dirPaths.push(path.join(baseDir, 'src/helpers'));
+        fullFileName = `${fileName}Helper`;
         break;
       case 'lib':
         dirPaths.push(path.join(baseDir, 'src/lib'));
+        fullFileName = `${fileName}Lib`;
         break;
       case 'resources':
-        dirPaths.push(
-          path.join(baseDir, 'src/domains'),
-          path.join(baseDir, 'src/handlers'),
-          path.join(baseDir, 'src/repositories'),
-          path.join(baseDir, 'src/services')
-        );
-        break;
+        const resourceOptions = ['service', 'handler', 'domain', 'repository'];
+        for (const resourceOption of resourceOptions) {
+          await createFile(resourceOption, fileName);
+        }
+        return;
       default:
         console.error('Invalid option. Please use "service", "handler", "domain", "repository", "helpers", "lib", or "resources".');
         return;
@@ -64,12 +122,12 @@ export async function createFile(option: string, fileName: string) {
       await fs.ensureDir(dirPath);
 
       // Menentukan path lengkap untuk file
-      const filePath = path.join(dirPath, `${fileName}.ts`);
+      const filePath = path.join(dirPath, `${fullFileName}.ts`);
 
       // Menulis konten ke file
-      await fs.writeFile(filePath, `// File ${option.charAt(0).toUpperCase() + option.slice(1)}: ${fileName}`, 'utf-8');
+      await fs.writeFile(filePath, exampleCodes[option](fileName), 'utf-8');
 
-      console.log(`File ${option} sudah dibuat: ${fileName}.ts di ${dirPath}`);
+      console.log(`File ${option} sudah dibuat: ${fullFileName}.ts di ${dirPath}`);
     }
   } catch (err) {
     console.error(`Error creating file: ${err}`);
